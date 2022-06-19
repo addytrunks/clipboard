31ca7cab12a2e3628f469b9d18e5395515479207
Adhithya Srivatsan <addytrunks@gmail.com>
2022-06-19 13:31:22 +0530

    Snippets Section Implemented

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,12 @@ export default function Home() {
 
       <div className="bg-[url('/images/bg-header-desktop.png')] bg-no-repeat bg-contain">
         
-      {/* Hero */}
+        {/* Hero */}
         <section>
-          <div className='max-w-6xl mx-auto text-center mb-40 px-10 pt-16'>
+          <div className='section-container mb-40 px-10 pt-16'>
             <img src="images/logo.svg" className="mx-auto my-16" />
             
-            <h3 className='mb-8 text-4xl font-bold text-darkGrayishBlue md:text-5xl'>
+            <h3>
               A history of everything you copy
             </h3>
 
@@ -24,7 +24,7 @@ export default function Home() {
             </p>
 
             {/* Buttons */}
-            <div className='flex flex-col justify-center w-full space-y-6 text-xl text-white md:flex-row md:space-y-0 md:space-x-4'>
+            <div className='button-container'>
               <button className="p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80">
                 Download for IOS
               </button>
@@ -33,6 +33,22 @@ export default function Home() {
                 Download for Mac
               </button>
             </div>
+          </div>
+        </section>
+      
+        {/* Snippets */}
+        <section>
+          <div className='section-container my-15 px-10'>
+
+            <h3>
+              Keep track of your snippets
+            </h3>
+
+            <p className="max-w-3xl mx-auto mb-24 text-xl leading-9 text-center text-grayishBlue">
+              Clipboard instantly stores any item you copy in the cloud, meaning you
+              can access your snippets immediately on all your devices. Our Mac and
+              iOS apps will help you organize everything.
+            </p>
           </div>
         </section>
 
