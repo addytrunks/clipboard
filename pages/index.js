28019ca431d2b3bb2139d7bd382c48af25e00732
Adhithya Srivatsan <addytrunks@gmail.com>
2022-06-19 18:47:10 +0530

    Supercharge section implemented

--- pages/index.js
+++ pages/index.js
@@ -101,6 +101,43 @@ export default function Home() {
             <img src="/images/image-devices.png" className='mx-auto' />
           </div>
         </section>
+
+        {/* Supercharge */}
+        <section>
+          <div className='section-container my-20'>
+            <h3>Supercharge your workout</h3>
+            <p className="max-w-3xl mx-auto mb-16 text-xl leading-9 text-center text-grayishBlue">
+              We've got the tools to boost your productivity 
+            </p> 
+
+            <div className="flex flex-col items-center justify-between space-y-16 md:flex-row md:space-y-0 md:space-x-12">
+              <div class="flex flex-col items-center space-y-5">
+                <img src="images/icon-blacklist.svg" alt="" class="mb-6" />
+                <h5 className='mb-2 text-darkGrayishBlue text-2xl font-bold'>Create Blacklists</h5>
+                <p class="max-w-md text-grayishBlue">
+                  Easily search your snippets by content, category, web address,
+                  application, and more.
+                </p>
+              </div>
+
+              <div class="flex flex-col items-center space-y-5">
+                <img src="images/icon-text.svg" alt="" class="mb-6" />
+                <h5 className='mb-2 text-darkGrayishBlue text-2xl font-bold'>Plain Text Snippets</h5>
+                <p class="max-w-md text-grayishBlue">
+                  Remove unwanted formatting from copied text for a consistent look.
+                </p>
+              </div>
+
+              <div class="flex flex-col items-center space-y-5">
+                <img src="images/icon-preview.svg" alt="" class="mb-6" />
+                <h5 className='mb-2 text-darkGrayishBlue text-2xl font-bold'>Sneak Preview</h5>
+                <p class="max-w-md text-grayishBlue">
+                  Quick preview of all snippets on your Clipboard for easy access.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   )
