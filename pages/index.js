ee9c76a13f534bf3f3cf782bd7c66542aa93d1ac
Adhithya Srivatsan <addytrunks@gmail.com>
2022-06-19 18:50:37 +0530

    Footer,Refrences Section Implemented

--- pages/index.js
+++ pages/index.js
@@ -138,6 +138,105 @@ export default function Home() {
             </div>
           </div>
         </section>
+
+        {/* Refrences */}
+        <section>
+          <div
+            class="flex flex-col items-center justify-between max-w-6xl px-10 mx-auto space-y-16 my-44 md:flex-row md:space-y-0"
+          >
+            <img src="images/logo-google.png" alt="" />
+            <img src="images/logo-ibm.png" alt="" />
+            <img src="images/logo-microsoft.png" alt="" />
+            <img src="images/logo-hp.png" alt="" />
+            <img src="images/logo-vector-graphics.png" alt="" />
+          </div>
+        </section>
+        
+        {/* Bottom */}
+        <section>
+          <div class="section-container my-20">
+            <h3>Clipboard for iOS and MacOS</h3>
+            <p class="section-content mb-10 text-xl">
+              Available for free on the App Store. Download for Mac or iOS, sync
+              with iCloud and you're ready to start adding to your clipboard.
+            </p>
+
+            <div class="button-container">
+              <a
+                href="#"
+                class="p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80"
+              >
+                Download for iOS
+              </a>
+              <a
+                href="#"
+                class="p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80"
+              >
+                Download for Mac
+              </a>
+            </div>
+          </div>
+        </section>
+
+        {/* Footer */}
+        <footer className="bg-gray-50">
+          <div className="section-container">
+            <div className="flex flex-col items-center justify-between md:flex-row">
+
+              <img src="images/logo.svg" alt="" className="scale-50" />
+
+              <div
+                className="flex flex-col items-center justify-between flex-1 mb-10 space-y-6 md:flex-row md:mb-0 md:space-y-0 text-grayishBlue"
+              >
+
+                <div
+                  className="flex flex-col space-y-4 md:flex-row md:ml-24 md:space-x-24 md:space-y-0"
+                >
+                  <div className="flex flex-col space-y-4 text-center md:text-left">
+                    <div><a href="#" className="hover:text-strongCyan">FAQs</a></div>
+                    <div>
+                      <a href="#" className="hover:text-strongCyan">Contact Us</a>
+                    </div>
+                  </div>
+                  <div className="flex flex-col space-y-4 text-center md:text-left">
+                    <div>
+                      <a href="#" className="hover:text-strongCyan">Privacy Policy</a>
+                    </div>
+                    <div>
+                      <a href="#" className="hover:text-strongCyan">Press Kit</a>
+                    </div>
+                  </div>
+                  <div className="flex flex-col space-y-4 text-center md:text-left">
+                    <div>
+                      <a href="#" className="hover:text-strongCyan">Install Guide</a>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex justify-between w-32 py-1">
+                  <a href="#"
+                    ><img
+                      src="images/icon-facebook.svg"
+                      alt=""
+                      className="duration-200 ficon"
+                  /></a>
+                  <a href="#"
+                    ><img
+                      src="images/icon-twitter.svg"
+                      alt=""
+                      className="duration-200 ficon"
+                  /></a>
+                  <a href="#"
+                    ><img
+                      src="images/icon-instagram.svg"
+                      alt=""
+                      className="duration-200 ficon"
+                  /></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   )
